f1ca4ba22bb6d61b12ab5ab5218bcf1e50b3e85f
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-08 07:06:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  new_array=[]
+  @new_array=new_array
+end
+
+def array_with_two_elements
+  new_array=[1,2]
+  @new_array=new_array
+end 
+
+def first_element(array)
+  @array=array.first()
+end
+
+def third_element(array)
+  array[0][3]
+  @array=array
+end
+  
